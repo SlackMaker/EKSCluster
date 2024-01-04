1f1aa1037ddb31a34df1d60cb6bb0b41441d44f1
Marcos <marcospantazis@gmail.com>
2024-01-04 18:54:09 +0000

    EKS Cluster AWS

---
+++ terraform.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.7.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
